3ee3a40f25ee0dcaad010a9a9f7349c666474f47
DavidHigueraFerrez <david.higuera.ferrez@gmail.com>
2015-04-30 16:07:11 +0200

    Primera pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,7 +3,7 @@ exports.question = function (req, res){
 	res.render('/quizes/question', {pregunta: 'Capital de Italia'});
 };
 //GET /quizes/answer
-exports.answer = function(res, res){
+exports.answer = function(req, res){
 	if(req.query.respuesta === 'Roma'){
 		res.render('quizes/answer', {respuesta: 'Correcta'});
 	} else {
