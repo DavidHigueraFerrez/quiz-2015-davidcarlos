27005889f65abc7558649b7cb03f11a4422fbac4
DavidHigueraFerrez <david.higuera.ferrez@gmail.com>
2015-05-04 16:11:25 +0200

    Definitivo heroku

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var partials = require('express-partials');
+
 var routes = require('./routes/index');
+var autores= require('./routes/authors');
+
 
 
 var app = express();
@@ -13,18 +17,22 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(partials());
+
+
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(partials());
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/', routes);
+app.use('/authors', autores);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
